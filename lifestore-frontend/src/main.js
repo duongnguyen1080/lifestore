0eeef9f78749a22ee16dd18bef11201ee9a3cccf
Duong Nguyen <duongthuynguyen118@gmail.com>
2024-10-25 09:44:41 +0700

    Install Mixpanel SDK

--- lifestore-frontend/src/main.js
+++ lifestore-frontend/src/main.js
@@ -6,6 +6,13 @@ import './assets/tailwind.css'
 import mdiVue from 'mdi-vue/v3'
 import * as mdijs from '@mdi/js'
 
+import mixpanel from "mixpanel-browser";
+mixpanel.init("c8ecaa35478b649afbb6b6366339fedf", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
+
 const app = createApp(App)
 
 app.use(router)
